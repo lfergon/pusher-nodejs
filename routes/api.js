76d4ed9ac3ea13338fa8c9ef895bba7c03b98d81
Luis <lfergon@gmail.com>
2016-12-11 13:17:15 +0100

    cluster eu

--- routes/api.js
+++ routes/api.js
@@ -7,6 +7,7 @@ const pusher = new Pusher({
   appId      : process.env.PUSHER_APP_ID,
   key        : process.env.PUSHER_APP_KEY,
   secret     : process.env.PUSHER_APP_SECRET,
+  cluster    : 'eu',
   encrypted  : true,
 });
 const channel = 'events_to_be_shown';
